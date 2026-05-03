a53c81d4318ca253ad303c8cbe3e6124fa056817
Sumit yadav <sumitkumaryadav9608700@gmail.com>
2026-05-03 20:25:51 +0530

    server file all most complted

---
+++ Frontend/src/features/Auth/Services/auth.api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+
+const api = axios.create({
+   baseURL: "http://localhost:3005",
+   withCredentials: true,
+})
+
+export async function register({username, email, password}) {
+  const response = await api.post("/api/auth/register", {
+      username, email, password
+  })
+
+  return response.data;
+  
+}
